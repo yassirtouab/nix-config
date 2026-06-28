ccac14b0530f81d856790adbe6d8f7df280f9fcc
Yassir Touab <contact@yassirtouab.com>
2026-06-28 20:34:59 +0100

    fix: force GTK style for QT apps to resolve stylix conflict

--- home-manager/modules/qt.nix
+++ home-manager/modules/qt.nix
@@ -1,14 +1,14 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   home.packages = with pkgs; [
     papirus-icon-theme
     pcmanfm-qt
   ];
   qt = {
     enable = true;
-    platformTheme.name = "gtk";
+    platformTheme.name = lib.mkForce "gtk";
     style = {
-      package = pkgs.adwaita-qt;
-      name = "adwaita-dark";
+      package = lib.mkForce pkgs.adwaita-qt;
+      name = lib.mkForce "adwaita-dark";
     };
   };
 }
