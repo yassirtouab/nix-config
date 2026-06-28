ff1f43579b45d2a3e57637a4b18a2da035b82072
Yassir Touab <contact@yassirtouab.com>
2026-06-28 14:07:00 +0100

    feat: symlink japanese_aesthetic.conf to theme.conf for SDDM astronaut theme

--- nixos/modules/sddm.nix
+++ nixos/modules/sddm.nix
@@ -12,6 +12,7 @@ let
     installPhase = ''
       mkdir -p $out/share/sddm/themes/sddm-astronaut
       cp -r * $out/share/sddm/themes/sddm-astronaut/
+      cp $out/share/sddm/themes/sddm-astronaut/Themes/japanese_aesthetic.conf $out/share/sddm/themes/sddm-astronaut/theme.conf
     '';
   };
 in {
