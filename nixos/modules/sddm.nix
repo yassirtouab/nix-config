ef403cb66b4fb29d54e0de165a0df177510cac67
Yassir Touab <contact@yassirtouab.com>
2026-06-28 12:42:21 +0100

    feat: add nixpkgs-unstable input and update SDDM to use unstable packages

--- nixos/modules/sddm.nix
+++ nixos/modules/sddm.nix
@@ -1,11 +1,19 @@
-{ pkgs, lib, ... }: {
+{ pkgs, lib, inputs, ... }:
+let
+  unstable-pkgs = import inputs.nixpkgs-unstable {
+    system = pkgs.system;
+    config.allowUnfree = true;
+  };
+in {
   # Enable Wayland-native SDDM
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    package = unstable-pkgs.kdePackages.sddm;
     theme = "sddm-astronaut";
     extraPackages = [
-      pkgs.sddm-astronaut
+      unstable-pkgs.sddm-astronaut
+      unstable-pkgs.kdePackages.qtsvg
     ];
   };
 
