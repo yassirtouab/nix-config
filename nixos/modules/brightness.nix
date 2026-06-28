246035c6cd34343bc1117429dd87a943eacd77cc
Yassir Touab <contact@yassirtouab.com>
2026-06-28 15:00:08 +0100

    refactor: replace manual actkbd systemd service with declarative NixOS module and dynamic udev rules

--- nixos/modules/brightness.nix
+++ nixos/modules/brightness.nix
@@ -1,19 +1,24 @@
 { pkgs, ... }: {
-  # Write the actkbd configuration file directly to /etc/actkbd.conf
-  environment.etc."actkbd.conf".text = ''
-    224:key+rep:exec:${pkgs.brightnessctl}/bin/brightnessctl set 10%-
-    225:key+rep:exec:${pkgs.brightnessctl}/bin/brightnessctl set 10%+
-  '';
-
-  # Define a dedicated, non-templated systemd service that binds directly to your keyboard (event1)
-  systemd.services.actkbd-brightness = {
-    description = "Active Keyboard Daemon for Brightness Keys";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "systemd-udev-settle.service" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.actkbd}/bin/actkbd -d /dev/input/event1 -c /etc/actkbd.conf";
-      Restart = "always";
-      RestartSec = "5s";
-    };
+  # Enable the global keyboard daemon
+  services.actkbd = {
+    enable = true;
+    bindings = [
+      {
+        keys = [ 224 ]; # Screen Brightness Down
+        events = [ "key" "rep" ];
+        command = "${pkgs.brightnessctl}/bin/brightnessctl set 10%-";
+      }
+      {
+        keys = [ 225 ]; # Screen Brightness Up
+        events = [ "key" "rep" ];
+        command = "${pkgs.brightnessctl}/bin/brightnessctl set 10%+";
+      }
+    ];
   };
+
+  # Broaden the udev rule to launch actkbd on all event input devices,
+  # ensuring virtual ACPI and HP WMI hotkey buses are captured.
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="input", KERNEL=="event*", TAG+="systemd", ENV{SYSTEMD_WANTS}+="actkbd@%k.service"
+  '';
 }
