eb382cb154ef078933e7a9c812f4b096dd9ec6c5
Yassir Touab <contact@yassirtouab.com>
2026-06-28 20:16:48 +0100

    chore: upgrade nixpkgs, home-manager, and stylix to release 26.05

--- flake.nix
+++ flake.nix
@@ -3,16 +3,16 @@
 
   inputs = {
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     stylix = {
-      url = "github:danth/stylix/release-24.11";
+      url = "github:danth/stylix/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
