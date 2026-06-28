2eea5be707d3d8b2ff1c519d64c0835c71482e91
Yassir Touab <contact@yassirtouab.com>
2026-06-28 16:21:15 +0100

    feat: enable silent boot configuration and suppress verbose kernel logging

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,4 +1,17 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable silent boot and hide TTY transition text/garbage
+  boot.consoleLogLevel = 0;
+  boot.initrd.verbose = false;
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "boot.shell_on_fail"
+    "loglevel=3"
+    "rd.systemd.show_status=false"
+    "rd.udev.log_level=3"
+    "udev.log_priority=3"
+  ];
 }
