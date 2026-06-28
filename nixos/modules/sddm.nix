f9a652edf1fc73e4f488ad9d00e694ecc9b39be9
Yassir Touab <contact@yassirtouab.com>
2026-06-28 13:20:52 +0100

    refactor: extract sddm-astronaut-theme derivation and add to systemPackages for global accessibility

--- nixos/modules/sddm.nix
+++ nixos/modules/sddm.nix
@@ -4,6 +4,16 @@ let
     system = pkgs.system;
     config.allowUnfree = true;
   };
+
+  sddm-astronaut-theme = pkgs.stdenvNoCC.mkDerivation {
+    name = "sddm-astronaut-theme";
+    src = unstable-pkgs.sddm-astronaut.src;
+    dontBuild = true;
+    installPhase = ''
+      mkdir -p $out/share/sddm/themes/sddm-astronaut
+      cp -r * $out/share/sddm/themes/sddm-astronaut/
+    '';
+  };
 in {
   # Enable Wayland-native SDDM
   services.displayManager.sddm = {
@@ -12,20 +22,17 @@ in {
     package = pkgs.kdePackages.sddm;
     theme = "sddm-astronaut";
     extraPackages = [
-      (pkgs.stdenvNoCC.mkDerivation {
-        name = "sddm-astronaut-theme";
-        src = unstable-pkgs.sddm-astronaut.src;
-        dontBuild = true;
-        installPhase = ''
-          mkdir -p $out/share/sddm/themes/sddm-astronaut
-          cp -r * $out/share/sddm/themes/sddm-astronaut/
-        '';
-      })
+      sddm-astronaut-theme
       pkgs.kdePackages.qtsvg
       pkgs.kdePackages.qtmultimedia
       pkgs.kdePackages.qtvirtualkeyboard
     ];
   };
+
+  # Link the theme globally so SDDM can find it at /run/current-system/sw/share/sddm/themes/
+  environment.systemPackages = [
+    sddm-astronaut-theme
+  ];
 
   # Disable TTY autologin to let the graphical login manager take priority
   services.getty.autologinUser = lib.mkForce null;
