7f2488490005efcf05da6b874ba4e6b94034e4ef
Yassir Touab <contact@yassirtouab.com>
2026-06-29 02:31:46 +0100

    feat: configure automatic garbage collection and store optimization

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -3,5 +3,13 @@
     experimental-features = [ "nix-command" "flakes" ];
     extra-substituters = [ "https://noctalia.cachix.org" ];
     extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+    auto-optimise-store = true;
+  };
+
+  # Automatic garbage collection for system generations
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
   };
 }
